42ad9b7263c43de7c5840f3c77540814ce9d9561
Young Seo Kim <youngseokim7@gmail.com>
2025-04-03 17:12:59 +0900

    initialize database

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
+import sqlite3 from "sqlite3";
 
 const app = express();
 app.use(cors());
