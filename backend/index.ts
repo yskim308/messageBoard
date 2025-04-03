9e350214cd79da268b117150505ad52dae4efe84
Young Seo Kim <youngseokim7@gmail.com>
2025-04-03 16:25:51 +0900

    initial connection of front and backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import type { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 4000;
 
 app.get("/", (req: Request, res: Response) => {
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [message, setMessage] = useState<string>("default");
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/");
+      setMessage(response.data);
+      console.log(`the message from the server: ${message}`);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  });
   return (
     <div>
-      <p>home?</p>
+      <p className="text-3xl">{message}</p>
     </div>
   );
 }
