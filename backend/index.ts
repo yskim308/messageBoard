e75fd5dea8ab026df0d7090d8b848ec5adaf2a18
Young Seo Kim <youngseokim7@gmail.com>
2025-04-03 15:59:24 +0900

    initial express endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
