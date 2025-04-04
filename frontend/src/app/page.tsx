dfb17b428fa2e4a4b8c71d08570c7e3457babb85
Young Seo Kim <youngseokim7@gmail.com>
2025-04-04 15:16:32 +0900

    recieve messages from backend and update state

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,23 +2,41 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+export interface Message {
+  id: number;
+  author: string;
+  date: string;
+  message: string;
+  votes: number;
+}
+
 export default function Home() {
-  const [message, setMessage] = useState<string>("default");
+  const [messages, setMessages] = useState<Message[]>([]);
   const fetchData = async () => {
     try {
       const response = await axios.get("http://localhost:4000/");
-      setMessage(response.data);
-      console.log(`the message from the server: ${message}`);
-    } catch (e) {
+      const messageResponse: Message[] = response.data.data;
+      setMessages(messageResponse);
+      console.log(messageResponse);
+    } catch (e: any) {
       console.log(e.message);
     }
   };
+
   useEffect(() => {
     fetchData();
-  });
+  }, []);
   return (
     <div>
-      <p className="text-3xl">{message}</p>
+      {messages.map((message) => {
+        return (
+          <div key={message.id}>
+            <div>{message.author}</div>
+            <div>{message.date}</div>
+            <div>{message.message}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
