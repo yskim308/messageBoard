3d174b9ccaa225b57c9210a7e6ae8d04c479eaf4
Young Seo Kim <youngseokim7@gmail.com>
2025-04-04 16:00:18 +0900

    change date string to readable form

--- frontend/src/app/Card.tsx
+++ frontend/src/app/Card.tsx
@@ -4,12 +4,22 @@ interface CardProps {
   message: Message;
 }
 
+function dateToString(date: string): string {
+  const dateObj = new Date(date);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return dateObj.toLocaleDateString("en-US", options);
+}
+
 export default function Card({ message }: CardProps) {
   return (
     <div className="flex flex-col w-full my-2">
       <div className="flex font-bold">
         <div>@{message.author}</div>
-        <div className="font-thin ml-2">{message.date}</div>
+        <div className="font-thin ml-2">{dateToString(message.date)}</div>
       </div>
       <div>{message.message}</div>
       <div>{message.id}</div>
