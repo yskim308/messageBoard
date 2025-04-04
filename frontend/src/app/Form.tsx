678480c34171f706aeefaf6c217bcb43b1a1ef43
Young Seo Kim <youngseokim7@gmail.com>
2025-04-04 16:37:23 +0900

    implement form with state and put call to express backend

--- frontend/src/app/Form.tsx
+++ frontend/src/app/Form.tsx
@@ -1,9 +1,60 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import axios from "axios";
+
+interface FormDetails {
+  author: string;
+  message: string;
+}
+
 export default function Form() {
+  const [formDetails, setFormDetails] = useState<FormDetails>({
+    author: "",
+    message: "",
+  });
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      axios.put("http:locahost:4000/", formDetails);
+      console.log("form submitted succesfully");
+    } catch (error) {
+      console.error("uh oh, this are fucked", error);
+    }
+  };
+
   return (
     <div>
-      <form action="submit">
-        <input type="text" placeholder="username" required />
-        <input type="text" placeholder="message content" />
+      <form onSubmit={handleSubmit}>
+        <div className="flex-col">
+          <div>
+            <label htmlFor="username">@: </label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              placeholder="username"
+              autoComplete="off"
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setFormDetails({ ...formDetails, author: e.target.value });
+              }}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="message content"
+              name="content"
+              id="content"
+              autoComplete="off"
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setFormDetails({ ...formDetails, message: e.target.value });
+              }}
+              required
+            />
+          </div>
+        </div>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
