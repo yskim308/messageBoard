cfa87ceef11f3b7ea2d2d68b278ac59a90bb8c16
Young Seo Kim <youngseokim7@gmail.com>
2025-04-05 15:58:31 +0900

    remove default public files

--- frontend/src/app/Card.tsx
+++ frontend/src/app/Card.tsx
@@ -16,7 +16,7 @@ function dateToString(date: string): string {
 
 export default function Card({ message }: CardProps) {
   return (
-    <div className="flex flex-col w-full my-2">
+    <div className="flex flex-col w-full hover:bg-gray-100 px-3 py-6 rounded-xl">
       <div className="flex">
         <div className="font-bold font-mono">@{message.author}</div>
         <div className="font-thin ml-2 text-sm flex items-center">
