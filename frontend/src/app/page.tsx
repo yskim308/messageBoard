c4b7c5cbc08d8453e9d42089ebbfb6f33b7eeebc
Young Seo Kim <youngseokim7@gmail.com>
2025-04-07 17:20:29 +0900

    add select menu for order by

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,30 +16,35 @@ export interface Message {
 export default function Home() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [reload, setReload] = useState<boolean>(false);
+  const [sortBy, setSortBy] = useState<"date" | "vote">("date");
+
   const backendBaseUrl: string =
     process.env.NEXT_PUBLIC_EXPRESS_API ?? "http://localhost:4000";
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`${backendBaseUrl}/`);
-      const messageResponse: Message[] = response.data.data;
-      setMessages(messageResponse);
-      console.log(messageResponse);
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        console.log(e.message);
-      } else {
-        console.log("unknown error");
-      }
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${backendBaseUrl}/`);
+        const messageResponse: Message[] = response.data.data;
+        setMessages(messageResponse);
+        console.log(messageResponse);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          console.log(e.message);
+        } else {
+          console.log("unknown error");
+        }
+      }
+    };
     fetchData();
-  }, [reload]);
+  }, [reload, sortBy, backendBaseUrl]);
 
   const reloadOnSubmit = useCallback(() => {
     setReload((prev: boolean) => !prev);
   }, []);
+
+  const handleSortByChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(e.target.value as "date" | "vote");
+  };
 
   return (
     <div className="flex flex-col items-center md:w-3/5">
@@ -47,6 +52,18 @@ export default function Home() {
       <hr className="w-full" />
       <h1 className="mt-10">Comments</h1>
       <Form fetchOnSubmit={reloadOnSubmit} />
+      <div>
+        <label htmlFor="sortBy">order by:</label>
+        <select
+          name="sortBy"
+          id="sortBy"
+          value={sortBy}
+          onChange={handleSortByChange}
+        >
+          <option value="date">date</option>
+          <option value="vote">vote</option>
+        </select>
+      </div>
       <div className="h-screen overflow-scroll flex flex-col items-center">
         {messages.map((message) => {
           return <Card message={message} key={message.id} />;
